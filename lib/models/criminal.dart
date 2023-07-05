0baa412a72b3b1b8f40ab159afbbb549b7fa2924
Vinnakota Lakshmi sahithi <69851403+Sahithi2539@users.noreply.github.com>
2023-07-05 12:19:29 +0530

    Add files via upload

--- lib/models/criminal.dart
+++ lib/models/criminal.dart
@@ -1,1 +1,39 @@
+class Criminal {
+  String uid;
+  String? image;
+  String name;
+  int age;
+  int pincode;
+  final DateTime timestamp;
 
+  Criminal({
+    required this.uid,
+    this.image,
+    required this.name,
+    required this.age,
+    required this.pincode,
+    required this.timestamp,
+  });
+
+  Map<String, dynamic> toMap() {
+    return {
+      'uid': uid,
+      'image': image,
+      'name': name,
+      'age': age,
+      'pincode': pincode,
+      'timestamp': timestamp.millisecondsSinceEpoch,
+    };
+  }
+
+  factory Criminal.fromMap(Map<dynamic, dynamic> map) {
+    return Criminal(
+      uid: map['uid'],
+      image: map['image'],
+      name: map['name'],
+      age: map['age'] ?? 0,
+      pincode: map['pincode'] ?? 0,
+      timestamp: DateTime.fromMillisecondsSinceEpoch(map['timestamp']),
+    );
+  }
+}
